89bce21be6f76c8dbd3c246c41030e4eb4088ca6
YongXun <53288723+YongXun@users.noreply.github.com>
2020-05-05 14:05:35 +0800

    Update 13.js

--- 13.js
+++ 13.js
@@ -1,13 +1,11 @@
-class Solution:
-    def canThreePartsEqualSum(self, A: List[int]) -> bool:
-        sumArr = sum(A)
-        sumTemp = 0
-        count = 0
-        for item in A:
-            sumTemp += item
-            if sumTemp == sumArr//3:
-                count = count + 1
-                sumTemp = 0
-            if count == 3:
-                return True
-        return False
+var canThreePartsEqualSum = function(A) {
+				let sum = A.reduce((num1,num2)=>num1+num2);
+				let count = 0; //记录有多少次达到sum/3
+				let tempSum = 0;
+				for(let item of A){
+					tempSum += item;
+					if(tempSum == sum/3){count++;tempSum=0;}
+					if(count == 3){return true;}
+				}
+				return false;
+			};
