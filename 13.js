9d6e9c9a23bb7a3c947342c03e3c3a154e14223e
YongXun <53288723+YongXun@users.noreply.github.com>
2020-05-07 13:10:17 +0800

    将数组分成和相等的三个部分

--- 13.js
+++ 13.js
@@ -1,11 +1,11 @@
 var canThreePartsEqualSum = function(A) {
-				let sum = A.reduce((num1,num2)=>num1+num2);
-				let count = 0; //记录有多少次达到sum/3
-				let tempSum = 0;
-				for(let item of A){
-					tempSum += item;
-					if(tempSum == sum/3){count++;tempSum=0;}
-					if(count == 3){return true;}
-				}
-				return false;
-			};
+	let sum = A.reduce((num1,num2)=>num1+num2);
+	let count = 0; //记录有多少次达到sum/3
+	let tempSum = 0;
+	for(let item of A){
+		tempSum += item;
+		if(tempSum == sum/3){count++;tempSum=0;}
+		if(count == 3){return true;}
+	}
+	return false;
+};
